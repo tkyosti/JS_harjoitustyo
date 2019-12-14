093bfade0da333af8aaef04d2a645953d232f400
Evilcrane <58556101+Evilcrane@users.noreply.github.com>
2019-12-14 13:14:42 +0200

    Update App.js
    /> oli väärällä rivillä.

--- App.js
+++ App.js
@@ -29,8 +29,7 @@ class App extends Component {
         
         return (
             <div className="container">
-                <Table characterData={characters} removeCharacter={this.removeCharacter} 
-        />
+                <Table characterData={characters} removeCharacter={this.removeCharacter} />
                 <Form handleSubmit={this.handleSubmit} />
             </div>
         )
